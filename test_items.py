d453a88f97532efe1281e0689b6d88103b013c9a
n.baranova <klin__77@mail.ru>
2023-01-06 13:03:21 +0300

    Assert для проверки кнопки "Добавить в корзину"

--- test_items.py
+++ test_items.py
@@ -1,8 +1,9 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-def test_button(browser):
-    browser.get(link)
-    browser.find_element(By.CLASS_NAME,'btn-add-to-basket')
 
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_exists_add_to_cart_button(browser):
+    browser.get(link)
+    buttons = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
+    assert len(buttons) == 1, 'На странице должна быть одна кнопка "Добавить в корзину"'
